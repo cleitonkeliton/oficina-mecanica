d2be6d51bfce0d409bc4a9ca54b718881edce155
Cleiton <cleitonkeliton.s@gmail.com>
2025-01-31 22:42:22 -0300

    Adicionando o README.md

--- src/main/servico/ServicoMecanico.java
+++
@@ -1,7 +0,0 @@
-public class ServicoMecanico extends Servico {
-    //construtor SUPER.
-    public ServicoMecanico(String descricaoSv, double valor) {
-        super(descricaoSv, valor);
-    }
-
-}
