259941fba0ded83bf682c54681c03a7e177fcc58
Cleiton Valdevino <155588167+cleitonkeliton@users.noreply.github.com>
2025-02-03 09:37:00 -0300

    Add files via upload

---
+++ Oficina.java
@@ -0,0 +1,37 @@
+package src.main.oficina;
+
+import src.main.servico.OrdemDeSv;
+import src.main.veiculo.Veiculo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Oficina {
+    private List<Cliente> clientes; // Corrigido o nome da lista
+    private List<Veiculo> veiculos;
+    private List<OrdemDeSv> ordemDeSvs;
+
+    public Oficina() {
+        this.clientes = new ArrayList<>();
+        this.veiculos = new ArrayList<>();
+        this.ordemDeSvs = new ArrayList<>();
+    }
+
+    public void adicionarCliente(Cliente cliente) {
+        clientes.add(cliente); // Adiciona o cliente à lista de clientes
+    }
+
+    public void adicionarVeiculo(Veiculo veiculo) {
+        veiculos.add(veiculo); // Adiciona o veículo à lista de veículos
+    }
+
+    public void adicionarOrdemServico(OrdemDeSv ordemDeSv) {
+        ordemDeSvs.add(ordemDeSv); // Adiciona a ordem de serviço à lista de ordens
+    }
+
+    public void listarOrdemSv() {
+        for (OrdemDeSv ordem : ordemDeSvs) {
+            System.out.println(ordem);
+        }
+    }
+}
