0c5a761ca9cf86d94cde8ff9cb5d518b4ebe7f97
Cleiton Valdevino <155588167+cleitonkeliton@users.noreply.github.com>
2025-02-04 09:45:11 -0300

    Add files via upload

---
+++ ServicoMecanico.java
@@ -0,0 +1,9 @@
+package src.main.servico;
+
+public class ServicoMecanico extends Servico {
+    //construtor SUPER.
+    public ServicoMecanico(String descricaoSv, double valor) {
+        super(descricaoSv, valor);
+    }
+
+}
